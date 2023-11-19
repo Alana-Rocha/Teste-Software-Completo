f9a661f009772bd3f4ea6c70cbefd66418a3efb9
Lucas Mendes <lucasespassini@gmail.com>
2023-11-19 18:12:28 -0300

    feat: adicionando codigo ruim para testar sonarcloud

--- src/teste-sonarcloud.ts
+++ src/teste-sonarcloud.ts
@@ -1,15 +1,41 @@
-const duplicada1 = (situacao: number) => {
-  if (situacao > 5 && situacao < 7) {
-    return true;
-  } else {
-    return false;
-  }
-};
+function calcularSoma(a, b) {
+  var resultado = a + b;
+  console.log("O resultado é: " + resultado);
+}
 
-const duplicada2 = (situacao: number) => {
-  if (situacao > 5 && situacao < 7) {
-    return true;
+function verificarNumeroPar(numero: string) { // forçando tipo errado
+  if (numero % 2 == 0) {
+    console.log("O número é par.");
   } else {
-    return false;
+    console.log("O número é ímpar.");
   }
-};
+}
+
+function exemploMásPraticas() {
+  var variavelNaoUsada;
+
+  for (var i = 0; i < 10; i++) {
+    console.log("Iteração " + i);
+  }
+
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    console.log("Loop infinito");
+  }
+
+  var array = [1, 2, 3];
+  console.log("Elemento inexistente: " + array[10]);
+
+  var objeto: any = {};
+  objeto.propriedade1 = 1;
+  objeto.propriedade2 = 2;
+  console.log("Propriedade inexistente: " + objeto.propriedade3);
+
+  var texto = "Isso é uma string muito longa que ultrapassa o limite permitido por boas práticas, então o SonarCloud pode apontar isso como um problema.";
+
+  calcularSoma(3, 4);
+
+  verificarNumeroPar(5);
+}
+
+exemploMásPraticas();
