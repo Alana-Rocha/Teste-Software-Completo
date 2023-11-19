7b17cfb2711a59a7ab8a2efd21db08fa01a8e86d
Lucas Mendes <lucasespassini@gmail.com>
2023-11-19 17:48:06 -0300

    fix

---
+++ src/teste-sonarcloud.ts
@@ -0,0 +1,15 @@
+const duplicada1 = (situacao: number) => {
+  if (situacao > 5 && situacao < 7) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const duplicada2 = (situacao: number) => {
+  if (situacao > 5 && situacao < 7) {
+    return true;
+  } else {
+    return false;
+  }
+};
