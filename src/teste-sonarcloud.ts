85b95a4dacf694d79087ad1f311f561df32e3ce3
Lucas Mendes <lucasespassini@gmail.com>
2023-11-19 18:20:17 -0300

    fix: retidando codigo ruim

--- src/teste-sonarcloud.ts
+++ src/teste-sonarcloud.ts
@@ -1,9 +1,9 @@
-function calcularSoma(a, b) {
+function calcularSoma(a: number, b: number) {
   var resultado = a + b;
   console.log("O resultado é: " + resultado);
 }
 
-function verificarNumeroPar(numero: string) { // forçando tipo errado
+function verificarNumeroPar(numero: number) {
   if (numero % 2 == 0) {
     console.log("O número é par.");
   } else {
@@ -12,26 +12,16 @@ function verificarNumeroPar(numero: string) { // forçando tipo errado
 }
 
 function exemploMásPraticas() {
-  var variavelNaoUsada;
-
   for (var i = 0; i < 10; i++) {
     console.log("Iteração " + i);
   }
 
-  // eslint-disable-next-line no-constant-condition
-  while (true) {
-    console.log("Loop infinito");
-  }
-
   var array = [1, 2, 3];
-  console.log("Elemento inexistente: " + array[10]);
+  console.log(array[3]);
 
   var objeto: any = {};
   objeto.propriedade1 = 1;
   objeto.propriedade2 = 2;
-  console.log("Propriedade inexistente: " + objeto.propriedade3);
-
-  var texto = "Isso é uma string muito longa que ultrapassa o limite permitido por boas práticas, então o SonarCloud pode apontar isso como um problema.";
 
   calcularSoma(3, 4);
 
